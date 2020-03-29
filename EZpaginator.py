4b84d77d3ffa7ff3f080a2c8021b8ac312ec8470
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-03-30 00:20:33 +0900

    Create EZpaginator.py

---
+++ EZpaginator.py
@@ -0,0 +1,90 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class YouLLamaed(Exception):
+    pass
+
+
+class Paginator:
+    def __init__(
+        self,
+        ctx,
+        message: discord.Message,
+        contents: list = None,
+        embeds: list = None,
+        timeout: int = 30,
+    ):
+        self.ctx = ctx
+        self.message = message  # Pagination Target
+        self.timeout = timeout
+        self.reactions = ["⬅️", "➡️"]
+        self.contents = contents
+        self.index = 0
+        self.embeds = embeds
+
+        if contents is None and embeds is None:
+            raise YouLLamaed("LLama ate all of your contents and embeds.")
+
+        if contents is None:
+            self.isEmbed = True
+        else:
+            self.isEmbed = False
+
+    def emoji_checker(self, payload):
+        if payload.member.bot:
+            return False
+
+        if payload.message_id != self.message.id:
+            return False
+
+        if str(payload.emoji) in self.reactions:
+            return True
+        return False
+
+    async def add_reactions(self):
+        for i in self.reactions:
+            await self.message.add_reaction(i)
+        return True
+
+    async def start(self):
+        await self.add_reactions()
+        while True:
+            try:
+                payload = await self.ctx.wait_for(
+                    "raw_reaction_add",
+                    check=self.emoji_checker,
+                    timeout=self.timeout,
+                )
+
+                await self.pagination(payload.emoji)
+            except asyncio.TimeoutError:
+                try:
+                    await self.message.clear_reactions()
+                except:
+                    pass
+
+    async def pagination(self, emoji):
+        if str(emoji) == "⬅️":
+            await self.go_previous()
+        elif str(emoji) == "➡️":
+            await self.go_next()
+
+    async def go_previous(self):
+        if self.index != 0:
+            self.index -= 1
+            if self.isEmbed:
+                await self.message.edit(embed=self.embeds[self.index])
+            else:
+                await self.message.edit(content=self.contents[self.index])
+
+    async def go_next(self):
+        if self.isEmbed:
+            if self.index != len(self.embeds) - 1:
+                self.index += 1
+                await self.message.edit(embed=self.embeds[self.index])
+
+        else:
+            if self.index != len(self.contents) - 1:
+                await self.message.edit(content=self.contents[self.index])
