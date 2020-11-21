05d566e74c99bf1778a0b71b32ac46649fcae401
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-11-21 10:45:45 +0900

    refactor: Fully rewrite

--- EZPaginator/EZPaginator.py
+++ EZPaginator/EZPaginator.py
@@ -1,50 +1,58 @@
 import asyncio
+from typing import List, Optional, Union
+
 import discord
 from discord.ext import commands
 
 
-class YouLLamaed(Exception):
-    pass
-
-
-class Submarine(Exception):
-    pass
+from EZPaginator.EZPaginator.exceptions import MissingAttributeException
 
 
 class Paginator:
     def __init__(
         self,
-        ctx,
+        bot: Union[
+            discord.Client,
+            discord.AutoShardedClient,
+            commands.Bot,
+            commands.AutoShardedBot,
+        ],
         message: discord.Message,
-        contents: list = None,
-        embeds: list = None,
+        contents: Optional[List[str]] = None,
+        embeds: Optional[List[discord.Embed]] = None,
         timeout: int = 30,
-        use_more: bool = False,
-        only: discord.abc.User = None,
-    ):
-        self.ctx = ctx
-        self.message = message  # Pagination Target
-        self.timeout = timeout
-        self.reactions = ["⬅️", "➡️"]
-        self.more_reactions = ["⏪", "⬅️", "➡️", "⏩"]
+        use_extend: bool = False,
+        only: Optional[discord.abc.User] = None,
+    ) -> None:
+        self.bot = bot
+        self.message = message
         self.contents = contents
-        self.index = 0
         self.embeds = embeds
-        self.use_more = use_more
+        self.timeout = timeout
+        self.use_extend = use_extend
         self.only = only
+        self.basic_emojis = ["⬅️", "➡️"]
+        self.extended_emojis = ["⏪", "⬅️", "➡️", "⏩"]
+        # TODO : 이모지 커스텀
+
+        if not (
+            isinstance(bot, discord.Client)
+            or isinstance(bot, discord.AutoShardedClient)
+            or isinstance(bot, commands.Bot)
+            or isinstance(bot, commands.AutoShardedBot)
+        ):
+            raise TypeError
 
         if contents is None and embeds is None:
-            raise YouLLamaed("LLama ate all of your contents and embeds.")
+            raise MissingAttributeException(
+                "Both contents and embeds are None."
+            )
 
-        if not isinstance(self.timeout, int):
-            raise Submarine("Your submarine tried to jump in a string.")
+        if not isinstance(timeout, int):
+            raise TypeError("timeout must be int.")
 
-        if self.only is not None:
-            if not isinstance(self.only, discord.abc.User):
-                raise TypeError
-
-    def emoji_checker(self, payload):
-        if payload.user_id == self.ctx.user.id:
+    def emoji_check(self, payload: discord.RawReactionActionEvent) -> bool:
+        if payload.user_id == self.bot.user.id:
             return False
 
         if payload.message_id != self.message.id:
@@ -54,36 +62,29 @@ class Paginator:
             if payload.user_id != self.only.id:
                 return False
 
-        if self.use_more:
-            if str(payload.emoji) in self.more_reactions:
-                return True
+        if self.use_extend:
+            if str(payload.emoji) not in self.extended_emojis:
+                return False
         else:
-            if str(payload.emoji) in self.reactions:
-                return True
-        return False
+            if str(payload.emoji) not in self.basic_emojis:
+                return False
 
-    async def add_reactions(self):
-        if self.use_more:
-            for i in self.more_reactions:
-                await self.message.add_reaction(i)
-        else:
-            for i in self.reactions:
-                await self.message.add_reaction(i)
         return True
 
-    async def start(self):
-        await self.add_reactions()
+    async def start(self) -> None:
+        await self.add_reaction()
 
         while True:
             try:
                 add_reaction = asyncio.ensure_future(
-                    self.ctx.wait_for(
-                        "raw_reaction_add", check=self.emoji_checker
+                    self.bot.wait_for(
+                        "raw_reaction_add", check=self.emoji_check
                     )
                 )
+
                 remove_reaction = asyncio.ensure_future(
-                    self.ctx.wait_for(
-                        "raw_reaction_remove", check=self.emoji_checker
+                    self.bot.wait_for(
+                        "raw_reaction_remove", check=self.emoji_check
                     )
                 )
 
@@ -97,63 +98,22 @@ class Paginator:
                     i.cancel()
 
                 if len(done) == 0:
-                    raise asyncio.TimeoutError()
+                    raise asyncio.TimeoutError
 
-                payload = done.pop().result()  ## done : set
-                await self.pagination(payload.emoji)
+                payload = done.pop().result()
+                await self.handle_pagination(payload.emoji)
 
-            except asyncio.TimeoutError:
-                try:
-                    await self.message.clear_reactions()
-                    break
-                except:
-                    break
+            except:
+                pass
 
-    async def pagination(self, emoji):
-        if str(emoji) == "⬅️":
-            await self.go_previous()
-        elif str(emoji) == "➡️":
-            await self.go_next()
+    async def add_reaction(self) -> None:
 
-        elif str(emoji) == "⏪":
-            await self.go_first()
-        elif str(emoji) == "⏩":
-            await self.go_last()
-
-    async def go_previous(self):
-        if self.index != 0:
-            self.index -= 1
-            if self.contents is None:
-                await self.message.edit(embed=self.embeds[self.index])
-            else:
-                await self.message.edit(content=self.contents[self.index])
-
-    async def go_next(self):
-        if self.contents is None:
-            if self.index != len(self.embeds) - 1:
-                self.index += 1
-                await self.message.edit(embed=self.embeds[self.index])
-
+        if self.use_extend:
+            for i in self.extended_emojis:
+                await self.message.add_reaction(i)
         else:
-            if self.index != len(self.contents) - 1:
-                self.index += 1
-                await self.message.edit(content=self.contents[self.index])
+            for i in self.basic_emojis:
+                await self.message.add_reaction(i)
 
-    async def go_first(self):
-        if self.index != 0:
-            self.index = 0
-            if self.contents is None:
-                await self.message.edit(embed=self.embeds[self.index])
-            else:
-                await self.message.edit(content=self.contents[self.index])
-
-    async def go_last(self):
-        if self.contents is None:
-            if self.index != len(self.embeds) - 1:
-                self.index = len(self.embeds) - 1
-                await self.message.edit(embed=self.embeds[self.index])
-
-        else:
-            if self.index != len(self.contents) - 1:
-                self.index = len(self.contents) - 1
-                await self.message.edit(content=self.contents[self.index])
+    async def handle_pagination(self, emoji: discord.PartialEmoji) -> None:
+        pass
---
+++ EZPaginator/exceptions.py
@@ -0,0 +1,2 @@
+class MissingAttributeException(Exception):
+    pass
