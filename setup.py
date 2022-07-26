b9b1f4f3cfec331910a6003420671fda871f671b
khk4912 <30457148+khk4912@users.noreply.github.com>
2022-07-27 00:40:21 +0900

    chore: Change python_requires & classifiers

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ setup(
     url="https://github.com/khk4912/EZPaginator",
     packages=find_packages(),
     keywords=["discord.py", "pagination"],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=["discord.py"],
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
